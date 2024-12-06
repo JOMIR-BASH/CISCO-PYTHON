60ac1b4206882c85bc3b97c9678009c9e8f5c4d4
JOSHUA <jmisaelriveracruz@gmail.com>
2024-12-05 23:00:50 -0600

    Uso de loops

--- src/Operadores/06-for.py
+++ src/Operadores/06-for.py
@@ -1,2 +1,14 @@
 for numero in range(5):
     print(numero, numero * "hola mundo ")
+
+buscar = 10
+for numero in range(5):
+    print(numero)
+    if numero == buscar:
+        print("Encontrado", buscar)
+        break
+else:
+    print("\"No se encontró el valor buscado\"")
+
+for char in "Ultimate python":
+    print(char)
