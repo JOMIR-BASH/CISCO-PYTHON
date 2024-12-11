19a210f32e7c62038932cdbc818f8ddb595fb872
JOSHUA <jmisaelriveracruz@gmail.com>
2024-12-10 23:43:37 -0600

    Combinación de while y condiconales

---
+++ src/Operadores/09b-condicionales.py
@@ -0,0 +1,11 @@
+# Condiconales
+
+
+while True:
+    numero = int(input("Digite un número: "))
+    if numero > 0:
+        print("El número es postivo")
+    elif numero == 0:
+        print("El número es cero")
+    else:
+        print("El número es negativo")
