c62bdda0364b84baaa5ffff96b29d8e82aa23169
JOSHUA <jmisaelriveracruz@gmail.com>
2024-12-18 21:13:03 -0600

    funciones

--- src/funciones/02-xargs.py
+++ src/funciones/02-xargs.py
@@ -2,10 +2,10 @@ def suma(*numeros):
     resultado = 0
     for numero in numeros:
         resultado += numero
-    print(resultado)   
+    print(resultado)
 
 
 suma(7, 1,)
 suma(7, 1, 10)
 suma(7, 1, 10, 20)
-
+suma(5, 10, 3, 14, 20)
