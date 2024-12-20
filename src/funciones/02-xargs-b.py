eb023c188196ae6710694de3a6d31ef92d1a7335
JOSHUA <jmisaelriveracruz@gmail.com>
2024-12-19 23:44:23 -0600

    xargs

---
+++ src/funciones/02-xargs-b.py
@@ -0,0 +1,24 @@
+def media(n1, n2, n3):
+    return (n1 + n2 + n3) / 3
+
+
+llamada = media(8, 8, 9)
+
+print(llamada)
+
+
+def imprimer_cosas():
+    print("Hola")
+    print("Mundo")
+    return
+    print("Python")
+
+
+imprimer_cosas()
+
+
+def resta(n1=1, n2=3):
+    return n1 - n2
+
+
+print(resta())
