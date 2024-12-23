7033900902d632e83db75c625e9016b4faeb9091
JOSHUA <jmisaelriveracruz@gmail.com>
2024-12-23 17:34:23 -0600

    Primeros pasos con listas

---
+++ src/tipo-avanzados/01-listas.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3]
+letras = ['a', 'b', 'c']
+palabras = ['hola', 'mundo']
+palabras_felices = ["feliz", "contento", "alegre"]
+booleans = [True, False, True]
+matriz = [[0, 1], ["x", "z"]]
+ceros = [0] * 10
+alfanumerico = numeros + letras
+rango = list(range(1, 11))
+chars = list("Hola mundo")
+
+print(chars)
---
+++ src/tipo-avanzados/02-manipulando-listas.py
@@ -0,0 +1,13 @@
+mascotas = ['perro', 'gato', 'pez', 'conejo', 'loro']
+print(mascotas[0])
+mascotas[0] = "dragon"
+# print(mascotas)
+# print(mascotas[0:2])
+# print(mascotas[2:])
+# print(mascotas[-1])
+# print(mascotas[2::2])
+
+numeros = list(range(21))
+
+print(numeros[0::2])
+print(numeros[1::2])
